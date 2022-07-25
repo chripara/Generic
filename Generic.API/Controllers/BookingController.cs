fa20defe0232456b9dd57ebed0dbef57d95ee777
Christos Paraskevas <45561681+chripara@users.noreply.github.com>
2022-07-25 11:25:30 +0300

    Booking controller create

---
+++ Generic.API/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Generic.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+
+        public BookingController()
+        {
+            
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooking()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBookings()
+        {
+            return Ok();
+        }
+    }
+}
