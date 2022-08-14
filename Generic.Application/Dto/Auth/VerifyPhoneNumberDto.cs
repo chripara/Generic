12ca03dbabc70f9557ac57e6edd82dfeadd45d81
Christos Paraskevas <45561681+chripara@users.noreply.github.com>
2022-08-14 10:36:12 +0300

    VerifyPhoneNumberDto created

---
+++ Generic.Application/Dto/Auth/VerifyPhoneNumberDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic.Application.Dto.Auth
+{
+    public class VerifyPhoneNumberDto
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
