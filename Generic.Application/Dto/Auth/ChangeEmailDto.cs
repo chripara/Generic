5a1ad06a4773cbe1d50530d3ff8dd559349da880
Christos Paraskevas <45561681+chripara@users.noreply.github.com>
2022-08-14 15:06:48 +0300

    ChangeEmailDto create

---
+++ Generic.Application/Dto/Auth/ChangeEmailDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic.Application.Dto.Auth
+{
+    public class ChangeEmailDto
+    {
+        public string CurrentEmail { get; set; }
+        public string NewEmail { get; set; }
+    }
+}
