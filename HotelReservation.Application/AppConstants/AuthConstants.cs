1dc33a7868f53e1e1c9567b5998dfd00ddf51aa4
Christos Paraskevas <45561681+chripara@users.noreply.github.com>
2022-08-30 14:03:41 +0300

    AuthConstants and RoleConstants static classes added

---
+++ HotelReservation.Application/AppConstants/AuthConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelReservation.Application.AppConstants
+{
+    public static class AuthConstants
+    {
+        public const int PhoneVerificationTokenDigits = 6;
+        public const string DefaultPhone = "+306955954852";
+        public const string DefaultEmail = "zarmerdic@gmail.com";        
+    }
+}
---
+++ HotelReservation.Application/AppConstants/RoleConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelReservation.AppConstants
+{
+    public static class RoleConstants
+    {
+        public const string RoleAdmin = "Admin";
+        public const string RoleUser = "User";
+    }
+}
