b8ef3ec7cd590f2cd23b2913804bed78a7fd8c04
Christos <cparaskevas@gmail.com>
2023-02-20 20:44:59 +0200

    Config added

--- src/HotelReservation/reactRootView/config/colors.js
+++ src/HotelReservation/reactRootView/config/colors.js
@@ -1,6 +1,8 @@
 export default {
     primary: "#E75874",
     secondary: "#FBCBC9",
+    third: "#BE1558",
+    brown: "32251440",
     black: "#000000",
     white: "#FFFFFF",
     light: "#D9D9D9",
